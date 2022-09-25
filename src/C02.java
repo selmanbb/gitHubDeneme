ec239a0564744773ff1cf23724c4d540c00db818
“selmanbb” <nurkasabali45@gmail.com>
2022-09-25 16:46:28 +0200

    C03

--- src/C02.java
+++ src/C02.java
@@ -1,5 +1,6 @@
 public class C02 {
     public static void main(String[] args) {
         System.out.println("deneme1" );
+        System.out.println("Deneme2");
     }
 }
---
+++ src/C03.java
@@ -0,0 +1,6 @@
+public class C03 {
+    public static void main(String[] args) {
+
+        System.out.println("deneme 3 MÜ??");
+    }
+}
