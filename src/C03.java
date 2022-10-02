1648e333b69635fb04ec0809dd18591315811b77
selmanbb <nurkasabali45@gmail.com>
2022-10-02 16:01:27 +0200

    C04

--- src/C03.java
+++ src/C03.java
@@ -2,5 +2,6 @@ public class C03 {
     public static void main(String[] args) {
 
         System.out.println("deneme 3 MÜ??");
+        System.out.println("pazar calismasi2");
     }
 }
---
+++ src/C04.java
@@ -0,0 +1,5 @@
+public class C04 {
+    public static void main(String[] args) {
+        System.out.println("güncellemedeki problemi cözmeye calisiyorum");
+    }
+}
